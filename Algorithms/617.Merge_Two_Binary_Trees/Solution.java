e8efb5151228579a14f7316bb80e675f8981d768
Andrew <hqpan.phq@gmail.com>
2019-10-18 11:11:00 +0800

    Add LeetCode 617 and renew the content.

---
+++ Algorithms/617.Merge_Two_Binary_Trees/Solution.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// Approach 1: Recursion
+Recursion 
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t2 == null)
+            return t1;
+        if (t1 == null)
+            return t2;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
+
+// Approach 2 : Iteration
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null)                             
+            return t2;
+        Stack<TreeNode[]> stack = new Stack<>();
+        stack.push(new TreeNode[] {t1, t2});        
+        while (!stack.empty())                   
+        {
+            TreeNode[] temp = stack.pop();
+            if (temp[1] == null)		// 将节点压入堆栈前，已确保temp[0]不为空；
+                continue;
+            temp[0].val += temp[1].val;
+            if (temp[0].left == null)
+                temp[0].left = temp[1].left;
+            else
+                stack.push(new TreeNode[] {temp[0].left, temp[1].left});
+
+            if (temp[0].right == null)
+                temp[0].right = temp[1].right;
+            else
+                stack.push(new TreeNode[] {temp[0].right, temp[1].right});    
+        }
+        return t1;
+    }
+}
--- README.md
+++ README.md
@@ -16,6 +16,7 @@
 - [268. Missing Number](./Algorithms/268.Missing_Number/note.md).[[Code](./Algorithms/268.Missing_Number/Solution.java)].
 - [287. Find the Duplicate Number](./Algorithms/287.Find_the_Duplicate_Number/note.md).[[Code](./Algorithms/287.Find_the_Duplicate_Number/Solution.java)].
 - [448. Find All Numbers Disappeared in an Array](./Algorithms/448.Find_All_Numbers_Disappeared_in_an_Array/note.md).[[Code](./Algorithms/448.Find_All_Numbers_Disappeared_in_an_Array/Solution.java)].
+- [617. Merge Two Binary Trees](./Algorithms/617.Merge_Two_Binary_Trees/note.md).[[Code](./Algorithms/617.Merge_Two_Binary_Trees/Solution.java)].
 - [645. Set Mismatch](./Algorithms/645.Set_Mismatch/note.md).[[Code](./Algorithms/645.Set_Mismatch/Solution.java)].
 - [1184. Distance Between Bus Stops](./Algorithms/1184.Distance_Between_Bus_Stops/note.md).[[Code](./Algorithms/1184.Distance_Between_Bus_Stops/Solution.java)].
 - [Weekly Contest Notes](./Algorithms/Weekly_Contest/note.md).
