bedf056823bba39d53e7ffc555627a564af9cd1f
hqpan <hqpan@foxmail.com>
2020-06-25 21:10:21 +0800

    Add Offer 15.

--- Algorithms/461.Hamming_Distance/Solution.java
+++ Algorithms/461.Hamming_Distance/Solution.java
@@ -1,4 +1,3 @@
-// Approach 1: 逐次去除最右侧的1
 class Solution {
     public int hammingDistance(int x, int y) {
         if (x < 0 || y < 0)
@@ -10,14 +9,5 @@ class Solution {
             xor &= xor - 1;            
         }
         return count;
-    }
-}
-
-// Approach 2: 使用内建方法 bitCount
-class Solution {
-    public int hammingDistance(int x, int y) {
-        if (x < 0 || y < 0)
-            throw new RuntimeException("x and y should be positive.");
-        return Integer.bitCount(x ^ y);
     }
 }
